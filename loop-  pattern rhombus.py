0961cbe05b70a5fb4a4769a72dca0c4b811226dd
chethangowda04 <chethanramaraju@gmail.com>
2024-07-20 22:20:23 +0530

    for loop

---
+++ loop-  pattern rhombus.py
@@ -0,0 +1,24 @@
+#nesting of loop
+#rhombus pattern
+for r in range(1,6):
+    for c in range(1,r+1):
+        print(c, end=" ")
+    print()
+
+for r in range(6,1,-1):
+    for c in range(1,r+1):
+        print(c, end=" ")
+    print()
+for r in range(6,1,-1):
+    for c in range(1,r-1,-1):
+        print(c, end=" ")
+    print()
+
+for r in range(1,6):
+    for c in range(1,r+1):
+        print("*", end="  ")
+    print()
+for r in range(6,1,-1):
+    for c in range(1,r):
+        print("*", end="  ")
+    print()
