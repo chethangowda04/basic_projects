c065e96d88c3188201da8a8ab5e163d4086edd88
chethangowda04 <chethanramaraju@gmail.com>
2024-07-30 22:59:48 +0530

    condition statement

---
+++ designation based on exeriance.py
@@ -0,0 +1,17 @@
+
+print("employee details based on the experience")
+experiance = int(input("enter the experiance of employee::"))
+print(experiance)
+while experiance
+    if experiance<=1:
+        print("Trainee")
+    elif  experiance>1 and experiance<=2:
+        print("chief executive officer")
+    elif  experiance>2 and experiance<=3:
+        print("team leader")
+    elif  experiance>3 and experiance<=4:
+        print("manager")
+    elif experiance>=5:
+        print("CEO")
+    else:
+        print("enter correct experience")
