99ca0af14d5b15c83210d4da04a649cba5b42342
chethangowda04 <chethanramaraju@gmail.com>
2024-07-31 21:34:12 +0530

    comprehensions

--- list comprehension.py
+++ list comprehension.py
@@ -53,3 +53,43 @@ list1 = [1,2,3]
 list2 = ["a", "b", "c"]
 pairs = [(x,y) for x in list1 for y in list2]
 print(pairs)
+
+
+
+#1. generate a set of common elements from two sets
+list1 = [1,2,3,4,5]
+list2 = [3,4,5,6,7]
+common = [i for i in list1 for j in list2 if i ==j]
+print(common)
+
+
+# #2. WAP to generate pair of distinct elements and their character
+list1 = ['abc', 'def', 'ghi']
+list2 = ['jkl', 'mno', 'pqr']
+pairs = [(i,j) for i in list1 for j in list2]
+print(pairs)
+print(ord())
+
+
+
+#3. create pairs of distinct elements and their absolute difference from two
+list1 = [3,6,9]
+list2 = [5,10,15]
+dis = [(i,j) for i in list1 for j in list2]
+difference = [abs(i-j) for i in list1 for j in list2]
+print(dis)
+print("difference::", difference)
+
+
+#4. write a list comprehension to display numbers and their binary representation from 1 to 10
+binary = [bin(n) for n in range (1,11)]
+print(binary)
+
+
+
+
+# #5.write a list comprehension to display numbers and their factorial from 1 to 10
+
+import math
+fac = [math.factorial(x) for x in range(1,11)]
+print(fac)
